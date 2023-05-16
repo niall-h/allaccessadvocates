71a639a89d6d0fee58ff4c86449a4f56cb4c4d70
Niall Htet <niallhtet@Nialls-MacBook-Pro.local>
2023-05-16 16:47:25 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Map, { Layer, Marker } from "react-map-gl";
+import markerImage from "./3dmarker.png";
 
-function App() {
+const mapboxAccessToken =
+  "pk.eyJ1Ijoibmh0ZXQiLCJhIjoiY2xobzJhdHZmMXI4cTNtbHIyNWYwcHZpdCJ9.OBYsMDOF2Ns3Pnqt_p-i9Q";
+
+export default function App() {
+  const bounds = [
+    [-117.2566, 32.865],
+    [-117.2103, 32.885],
+  ];
+
+  const buildingsLayer = {
+    id: "add-3d-buildings",
+    source: "composite",
+    "source-layer": "building",
+    filter: ["==", "extrude", "true"],
+    type: "fill-extrusion",
+    minzoom: 11,
+    paint: {
+      "fill-extrusion-color": "#aaa",
+
+      // Use an 'interpolate' expression to
+      // add a smooth transition effect to
+      // the buildings as the user zooms in.
+      "fill-extrusion-height": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        15,
+        0,
+        15.05,
+        ["get", "height"],
+      ],
+      "fill-extrusion-base": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        15,
+        0,
+        15.05,
+        ["get", "min_height"],
+      ],
+      "fill-extrusion-opacity": 0.6,
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      initialViewState={{
+        longitude: -117.23,
+        latitude: 32.8776,
+        zoom: 14.67,
+      }}
+      style={{ width: "100%", height: "80vh" }}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapboxAccessToken={mapboxAccessToken}
+      pitch={50}
+      maxBounds={bounds}
+      antialias={true}
+    >
+      <Layer {...buildingsLayer} />
+      <Marker
+        longitude={-117.23}
+        latitude={32.8776}
+        anchor="bottom"
+        pitchAlignment="viewport"
+      >
+        <img
+          src={markerImage}
+          alt="marker"
+          style={{
+            height: "60px",
+            width: "60px",
+          }}
+        />
+      </Marker>
+    </Map>
   );
 }
-
-export default App;
---
+++ src/Map.js
@@ -0,0 +1,97 @@
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoibmh0ZXQiLCJhIjoiY2xobzJhdHZmMXI4cTNtbHIyNWYwcHZpdCJ9.OBYsMDOF2Ns3Pnqt_p-i9Q";
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-117.23);
+  const [lat, setLat] = useState(32.8776);
+  const [zoom, setZoom] = useState(14.67);
+  const bounds = [
+    [-117.2566, 32.865],
+    [-117.2103, 32.885],
+  ];
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+      pitch: 50,
+      maxBounds: bounds,
+      antialias: true,
+    });
+
+    map.current.on("style.load", () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.current.getStyle().layers;
+      const labelLayerId = layers.find(
+        (layer) => layer.type === "symbol" && layer.layout["text-field"]
+      ).id;
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      map.current.addLayer(
+        {
+          id: "add-3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 11,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // Use an 'interpolate' expression to
+            // add a smooth transition effect to
+            // the buildings as the user zooms in.
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "height"],
+            ],
+            "fill-extrusion-base": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "min_height"],
+            ],
+            "fill-extrusion-opacity": 0.6,
+          },
+        },
+        labelLayerId
+      );
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container"></div>
+    </div>
+  );
+}
